ee53c91e32f8095f73c795fa0b6e2698a78d6fa6
Getshi <getshi@gmx.at>
2015-12-12 12:34:30 +0100

    lay minigame init

--- source/Minigame_Lay.hx
+++ source/Minigame_Lay.hx
@@ -1,9 +1,13 @@
 package;
 
 import flixel.*;
+import flixel.ui.FlxBar;
 
 class Minigame_Lay implements Minigame {
 	private var state:PlayState;
+	private var powerbar:FlxBar;
+	private var power:Float;
+	//add barsprite to state in init and remove in destroy
 
 	public function new():Void
 	{
@@ -11,11 +15,14 @@ class Minigame_Lay implements Minigame {
 	}
 	public function init():Void
 	{
-
+		power = 0;
+		powerbar = new(0, 240, FlxBar.FILL_LEFT_TO_RIGHT, 100, 10, this, "power", 0, 100, false);
+		powerbarcreate.GradientBar([0xff005555], [0xffff0000, 0xff00ff00], 2, 180, false, 0xffffffff);
 	}
 
 	public function update():Void
 	{
-
+		power += 0.01;
+		power = power % 100;
 	}
 }

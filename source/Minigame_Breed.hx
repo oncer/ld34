7b3ea3b377a1ea7ccddaf07fb7ba029686a0a72e
Simon Parzer <simon.parzer@gmail.com>
2015-12-12 12:03:02 +0100

    minigame classes

---
+++ source/Minigame_Breed.hx
@@ -0,0 +1,19 @@
+package;
+
+class Minigame_Breed implements Minigame {
+	private var state:PlayState;
+
+	public function new():Void
+	{
+		state = cast(FlxG.state, PlayState);
+	}
+	public function init():Void
+	{
+
+	}
+
+	public function update():Void
+	{
+
+	}
+}
---
+++ source/Minigame_Hatch.hx
@@ -0,0 +1,19 @@
+package;
+
+class Minigame_Hatch implements Minigame {
+	private var state:PlayState;
+
+	public function new():Void
+	{
+		state = cast(FlxG.state, PlayState);
+	}
+	public function init():Void
+	{
+
+	}
+
+	public function update():Void
+	{
+
+	}
+}

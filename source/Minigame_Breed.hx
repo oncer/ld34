eeed58695522bb3c02b368e4701fb1ec4e0aa0dc
Getshi <getshi@gmx.at>
2015-12-12 19:31:15 +0100

    thermo gfx

--- source/Minigame_Breed.hx
+++ source/Minigame_Breed.hx
@@ -5,7 +5,8 @@ import flixel.util.*;
 
 class Minigame_Breed implements Minigame {
 	private var state:PlayState;
-	private var dbgSlider:FlxSprite;
+	private var thermoSlider:FlxSprite;
+	private var thermoInd:FlxSprite;
 
 	private var size:Float;
 	private var temperature:Float; // -1 . 0 . 1
@@ -29,14 +30,18 @@ class Minigame_Breed implements Minigame {
 		FlxG.watch.add(this, "tchange");		
 		FlxG.watch.add(this, "size");
 
-		dbgSlider = new FlxSprite(240, 450);
-		dbgSlider.makeGraphic(20, 20, 0xffffff00);
-		dbgSlider.offset.x = 10;
-		dbgSlider.offset.y = 10;
+		thermoSlider = new FlxSprite(240, 440, "assets/images/thermometer.png");
+		thermoSlider.offset.x = 144;
+		thermoSlider.origin.x = 144;
+		thermoInd = new FlxSprite(240, 410, "assets/images/thermometer_indicator.png");
+		thermoInd.offset.x = 16;
+		thermoInd.origin.x = 16;
+		//thermoInd.offset = thermoInd.origin;
+		state.add(thermoSlider);
+		state.add(thermoInd);
 
 		state.chicken.x = 240;
 		state.chicken.y = 480 * Backdrop.HORIZON;
-		state.add(dbgSlider);
 
 		state.egg.size = 0.33;
 
@@ -45,7 +50,8 @@ class Minigame_Breed implements Minigame {
 
 	public function destroy():Void
 	{
-		state.remove(dbgSlider);
+		state.remove(thermoSlider);
+		state.remove(thermoInd);
 	}
 
 	public function update():Void
@@ -53,7 +59,7 @@ class Minigame_Breed implements Minigame {
 		state.chicken.y = 480 * Backdrop.HORIZON - state.egg.offset.y * state.egg.size;
 
 		temperature = Math.min(1, Math.max(-1, temperature + tchange));
-		if (temperature < -0.99) tchange = Math.max(0, tchange);
+		if (temperature < -0.99 && FlxG.keys.pressed.SPACE) tchange = Math.max(0, tchange); //added keypress necessary --> slider can go all the way to the left
 		temperature -= 0.03*(1.0 - temperature*temperature);
 
 		echange = (1 - Math.abs(temperature)) * 0.003;
@@ -66,7 +72,8 @@ class Minigame_Breed implements Minigame {
 		}
 		tchange = Math.max(-0.05, Math.min(0.05, tchange));
 
-		dbgSlider.x = temperature * 240 + 240;
+		thermoInd.x = temperature * 136 + 240;
+		trace (thermoInd.x);
 
 		if (timer.finished) {
 			state.nextMinigame();

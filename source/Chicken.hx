441493639348cd20aeeade12fcbc9f67739cf33d
Getshi <getshi@gmx.at>
2015-12-12 22:09:06 +0100

    changes to lay and breed

--- source/Chicken.hx
+++ source/Chicken.hx
@@ -11,14 +11,13 @@ class Chicken extends FlxSprite {
 		animation.add("idle", [0,1], 7, true);
 		animation.add("prepare", [2], 0, false);
 		animation.add("poop", [3], 0, false);
-		animation.add("wiggle", [4, 5], 15, true);
+		animation.add("wiggle", [4, 5], 10, true);
 		animation.play("idle");
 		offset = new FlxPoint(32, 59);
 		origin = offset;
 		setFacingFlip(FlxObject.LEFT, false, false);
 		setFacingFlip(FlxObject.RIGHT, true, false);
 		facing = FlxObject.RIGHT;
-		//animation.add("idle", [0, 1, 2, 3], 10, true);
 	}
 	
 	public function vibrate(x:Float, y:Float, strength:Float):Void {
--- source/Minigame_Breed.hx
+++ source/Minigame_Breed.hx
@@ -2,11 +2,13 @@ package;
 
 import flixel.*;
 import flixel.util.*;
+import flixel.ui.FlxBar;
 
 class Minigame_Breed implements Minigame {
 	private var state:PlayState;
 	private var thermoSlider:FlxSprite;
 	private var thermoInd:FlxSprite;
+	private var rocketfire:FlxSprite;
 
 	private var size:Float;
 	private var temperature:Float; // -1 . 0 . 1
@@ -14,6 +16,10 @@ class Minigame_Breed implements Minigame {
 	private var echange:Float; // egg size change
 
 	private var timer:FlxTimer; // egg timer
+	private var timer_gfxbg:FlxSprite;
+	private var timer_gfxfg:FlxSprite;
+	
+	private var substate:Int; // breeding | chicken flying away & zooming
 
 	public function new():Void
 	{
@@ -24,7 +30,8 @@ class Minigame_Breed implements Minigame {
 	{
 		size = 1;
 		temperature = 0.5;
-		tchange = 0;
+		tchange = 0.001;
+		substate = 0;
 
 		FlxG.watch.add(this, "temperature");
 		FlxG.watch.add(this, "tchange");		
@@ -36,9 +43,25 @@ class Minigame_Breed implements Minigame {
 		thermoInd = new FlxSprite(240, 410, "assets/images/thermometer_indicator.png");
 		thermoInd.offset.x = 16;
 		thermoInd.origin.x = 16;
-		//thermoInd.offset = thermoInd.origin;
+		rocketfire = new FlxSprite();
+		rocketfire.offset.x = 16;
+		rocketfire.origin.x = 16;
+		rocketfire.loadGraphic("assets/images/rocketfire.png", true, 32, 48);
+		rocketfire.animation.add("fire", [0,1,2,3], 7, true);
+		rocketfire.animation.play("fire");
+		rocketfire.kill();
+		timer_gfxbg = new FlxSprite(380, 96, "assets/images/eggtimer.png");
+		timer_gfxbg.centerOrigin();
+		timer_gfxbg.centerOffsets(true);
+		timer_gfxfg = new FlxSprite(396, 128);
+		timer_gfxfg.loadRotatedGraphic("assets/images/eggtimer_indicator.png", 180); //180 precomputed rotations
+		timer_gfxfg.centerOrigin();
+		timer_gfxfg.centerOffsets(true);
 		state.add(thermoSlider);
 		state.add(thermoInd);
+		state.add(rocketfire);
+		state.add(timer_gfxbg);
+		state.add(timer_gfxfg);
 
 		state.chicken.x = 240;
 		state.chicken.y = 480 * Backdrop.HORIZON;
@@ -52,31 +75,64 @@ class Minigame_Breed implements Minigame {
 	{
 		state.remove(thermoSlider);
 		state.remove(thermoInd);
+		state.remove(rocketfire);
+		state.remove(timer_gfxbg);
+		state.remove(timer_gfxfg);
 	}
 
 	public function update():Void
 	{
-		state.chicken.y = 480 * Backdrop.HORIZON - state.egg.offset.y * state.egg.size;
+		if (substate == 0) {
+			var cur_chicken_y = 480 * Backdrop.HORIZON - state.egg.offset.y * state.egg.size;
 
-		temperature = Math.min(1, Math.max(-1, temperature + tchange));
-		if (temperature < -0.99 && FlxG.keys.pressed.SPACE) tchange = Math.max(0, tchange); //added keypress necessary --> slider can go all the way to the left
-		temperature -= 0.03*(1.0 - temperature*temperature);
+			temperature = Math.min(1, Math.max(-1, temperature + tchange));
+			if (temperature < -0.99 && FlxG.keys.pressed.SPACE) tchange = Math.max(0, tchange); //added keypress necessary --> slider can go all the way to the left
+			if (temperature > 0.99 && !FlxG.keys.pressed.SPACE) tchange = Math.min(0, tchange); //.. same for other side
+			temperature -= 0.03*(1.0 - temperature*temperature);
 
-		echange = (1 - Math.abs(temperature)) * 0.003;
-		state.egg.size += echange;
+			echange = Math.max(0, (1 - 2 * Math.abs(temperature))) * 0.003; //changed --> bad temperatures no growth
+			state.egg.size += echange;
 
-		if (FlxG.keys.pressed.SPACE) {
-			tchange += 0.001;
+			if (FlxG.keys.pressed.SPACE) {
+				state.chicken.vibrate(240, cur_chicken_y, 2); //Loltest
+				tchange += 0.001;
+			} else {
+				state.chicken.x = 240;
+				state.chicken.y = cur_chicken_y;
+				tchange -= 0.001;
+			}
+			tchange = Math.max(-0.05, Math.min(0.05, tchange));
+
+			thermoInd.x = temperature * 136 + 240;
+			
+			timer_gfxfg.angle = 360 * timer.progress;
+			
+			if (timer.finished) {
+				substate = 1;
+				rocketfire.x = state.chicken.x;
+				rocketfire.y = state.chicken.y;
+				rocketfire.revive();
+				FlxG.sound.play("assets/sounds/rocket.wav");
+				FlxG.sound.play("assets/sounds/hngh.wav");//bogogck
+				timer_gfxbg.kill();
+				timer_gfxfg.kill();
+			}
+			
+			if (FlxG.keys.justPressed.SPACE) state.chicken.playAnimation("wiggle");
+			else if (FlxG.keys.justReleased.SPACE) state.chicken.playAnimation("idle");
+
 		} else {
-			tchange -= 0.001;
+			//TODO fly
+			state.chicken.playAnimation("idle");
+			state.chicken.y -= 5;
+			rocketfire.y -= 5;
+			//TODO zoom at constant speed (get zoomtime from current eggscale~ scale 1.5 needs less time than scale 2.5)
+			//-- chicken getting smaller
+			//-- bg zoom
+			//TODO after zoom:
+			//-- kill() chicken
+			//-- state.nextMinigame();
 		}
-		tchange = Math.max(-0.05, Math.min(0.05, tchange));
 
-		thermoInd.x = temperature * 136 + 240;
-		trace (thermoInd.x);
-
-		if (timer.finished) {
-			state.nextMinigame();
-		}
 	}
 }
